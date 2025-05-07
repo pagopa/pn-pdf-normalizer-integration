d97fd65b39365a262b5709a09c27fab2acc2b4a8
riccardo.pompili <riccardo.pompili@dgsspa.com>
2025-05-07 17:12:40 +0200

    PN-14870 - NormalizerTransformationCallback

--- functions/NormalizerTransformationCallback/src/app/eventHandler.js
+++ functions/NormalizerTransformationCallback/src/app/eventHandler.js
@@ -1,1 +1,99 @@
-exports.handleEvent = async (event) => {};
+"use strict";
+const {
+    S3Client,
+    CopyObjectCommand,
+    GetObjectTaggingCommand,
+    DeleteObjectCommand
+} = require("@aws-sdk/client-s3");
+const {
+    parseS3Uri
+} = require("@aws-sdk/util-uri-escape");
+
+const s3 = new S3Client();
+
+const BUCKET_NAME = "";
+const FILE_KEY = "";
+
+let checkResult = "";
+let mainErrorReason = "";
+let outputPath = "";
+let correlationId = "";
+
+let tagValue = "";
+
+exports.handleEvent = async function(event) {
+    try {
+        console.log("Received event:", JSON.stringify(event));
+
+        await Promise.allSettled(
+            event.Records.map(async (record) => {
+                    const bodyData = JSON.parse(record.body);
+
+                    checkResult = bodyData.checkResult;
+                    mainErrorReason = bodyData.mainErrorReason;
+                    outputPath = bodyData.outputPath;
+                    correlationId = bodyData.correlationId;
+
+
+
+                const parsedUri = parseS3Uri(outputPath);
+
+                if (parsedUri) {
+                    BUCKET_NAME = parsedUri.bucket;
+                    FILE_KEY = parsedUri.key;
+                } else {
+                    console.error("L'URI S3 non è valido:", outputPath);
+                }
+
+
+
+                const tagResponse = await s3.send(new GetObjectTaggingCommand({
+                    Bucket: BUCKET_NAME,
+                    Key: FILE_KEY
+                }));
+
+                const normalizationTag = tagResponse.TagSet.find(tag => tag.Key === "Transformation-NORMALIZATION");
+                if (!normalizationTag) {
+
+                    if (checkResult) {
+                        tagValue = "OK";
+                    } else {
+                        tagValue = "ERROR";
+                    }
+
+                    let tagSettings = {
+                        Bucket: BUCKET_NAME,
+                        Key: FILE_KEY,
+                        Tagging: {
+                            TagSet: [{
+                                Key: "Transformation-NORMALIZATION",
+                                Value: tagValue,
+                            }, ],
+                        }
+                    };
+
+                    const command = new PutObjectTaggingCommand(tagSettings);
+                    const response = await client.send(command);
+
+                    //continue;
+                }
+
+
+            })
+        );
+
+
+
+
+    return {
+        statusCode: 200,
+        body: "Process completed successfully!"
+    };
+} catch (error) {
+    console.error("Error:", error);
+    return {
+        statusCode: 500,
+        body: "Error during antivirus processing."
+    };
+}
+};
---
+++ functions/NormalizerTransformationCallback/src/test/eventHandler.test.js
@@ -0,0 +1,71 @@
+const { mockClient } = require("aws-sdk-client-mock");
+const { S3Client, GetObjectTaggingCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+const proxyquire = require("proxyquire").noPreserveCache();
+const { expect } = require("chai");
+
+const s3MockClient = mockClient(S3Client);
+
+describe("NormalizerTransformationCallback", () => {
+  let capturedTagging;
+  let fakeParseS3Uri;
+
+  beforeEach(() => {
+    // Mock di S3Client
+    s3MockClient = {
+      send: (command) => {
+        if (command instanceof GetObjectTaggingCommand) {
+          return Promise.resolve({
+            TagSet: [] // Simuliamo l'assenza di tag
+          });
+        } else if (command instanceof PutObjectTaggingCommand) {
+          capturedTagging = command;
+          return Promise.resolve({});
+        }
+        return Promise.reject(new Error("Unknown command"));
+      }
+    };
+
+    fakeParseS3Uri = {
+      parse: (uri) => {
+        return { bucket: "openshift-pam-bucket", key: "PAC/test.pdf" };
+      }
+    };
+  });
+
+  it("test ok", async () => {
+    const lambda = proxyquire("../app/eventHandler.js", {
+      "@aws-sdk/client-s3": { S3Client: s3MockClient },
+      "@aws-sdk/util-uri-escape": { parseS3Uri: fakeParseS3Uri.parse }
+    });
+
+    const fakeEvent = {
+      Records: [{
+        body: JSON.stringify({
+          checkResult: true,
+          mainErrorReason: "",
+          outputPath: "s3://openshift-pam-bucket/PAC/test.pdf",
+          correlationId: "abcdefg-hijkl"
+        }),
+        messageId: "msg-1"
+      }]
+    };
+
+    const result = await lambda.handleEvent(fakeEvent);
+
+    // Verifica che la risposta sia corretta
+    expect(result.statusCode).to.equal(200);
+    expect(result.body).to.equal("Process completed successfully!");
+
+    // Verifica che i tag siano stati aggiunti correttamente
+    expect(capturedTagging.Tagging.TagSet).to.deep.equal([{
+      Key: "Transformation-NORMALIZATION",
+      Value: "OK"
+    }]);
+  });
+
+  afterEach(() => {
+    delete process.env.BEYONDOC_API_URL;
+    delete process.env.PnSsGestoreRepositoryProtocol;
+    delete process.env.NORMALIZER_MARGINS;
+  });
+});
