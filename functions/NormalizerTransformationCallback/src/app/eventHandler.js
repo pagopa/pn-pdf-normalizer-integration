af2e0107dbad629252e94e5bba1d078be7090dd1
paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>
2026-01-29 16:05:00 +0100

    PN-18146: add check on inProgress tag status (#31)
    Co-authored-by: Martina Mattei <martinamattei.mm00@gmail.com>

--- functions/NormalizerTransformationCallback/src/app/eventHandler.js
+++ functions/NormalizerTransformationCallback/src/app/eventHandler.js
@@ -24,6 +24,11 @@ exports.handleEvent = async function (event) {
         console.log("Normalization CheckResult:", checkResult);
 
         const TAG_KEY = "Transformation-NORMALIZATION";
+        const TAG_VALUES = {
+            IN_PROGRESS: "inProgress",
+            OK: "OK",
+            ERROR: "ERROR"
+        };
 
         // Recupera i tag esistenti
         const tagResponse = await s3.send(new GetObjectTaggingCommand({
@@ -31,11 +36,18 @@ exports.handleEvent = async function (event) {
             Key: fileKey
         }));
 
-        // Cerca se il tag "Transformation-NORMALIZATION" è già presente
+        /* Cerca se il tag "Transformation-NORMALIZATION" è già presente:
+        controllo aggiunto per evitare di aggiornare i tag e quindi far partire nuove lavorazioni in caso di
+        chiamate multiple alla callback da parte di BeyonDoc.
+        A questo controllo è stato aggiunto il check sul valore "inProgress".
+        Questo è uno stato transitorio aggiunto con la catena di trasformazioni, per indicare la lavorazione tra una trasformazione
+        e l'altra. La condizione in || è necessaria affinchè il flusso di multi-trasformazione non si blocchi durante la fase
+        transitoria "inProgress".
+        */
         const normalizationTag = tagResponse.TagSet.find(tag => tag.Key === TAG_KEY);
 
 
-        if (!normalizationTag) {
+        if (!normalizationTag || normalizationTag.Value === TAG_VALUES.IN_PROGRESS) {
             const tagValue = checkResult ? "OK" : "ERROR";
             const tagSettings = {
                 Bucket: bucketName,
